0a63f694decf3f7a6c980733da01a5267b23cb93
Jeffrey <jeffrey.mata@mlhuillier.com>
2022-03-26 14:04:09 +0800

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,141 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { StyleSheet, Text, View,Image,Dimensions,TouchableOpacity } from 'react-native';
+import MapView, { Callout, Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import Swiper from 'react-native-swiper';
+import { } from 'react-native-web';
 
+import { markers } from './data/markers';
+
+const { width, height } = Dimensions.get("window");
+const CARD_HEIGHT = 220;
+const CARD_WIDTH = width * 0.8;
+const SPACING_FOR_CARD_INSET = width * 0.1 - 10;   
 export default function App() {
+ 
+  const [location, setlocation] = useState({
+    lat: markers[0].coordinate.lat,
+    lng:markers[0].coordinate.lng
+  })
+ 
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+  
+  <View style={styles.container}>
+      <MapView
+       // remove if not using Google Maps
+       style={styles.map}
+       region={{
+         latitude:location.lat,
+         longitude: location.lng,
+         latitudeDelta: 0.015,
+         longitudeDelta: 0.0121,
+       }}
+      >
+        {markers.map((val,i) => (
+          <Marker
+            key={i}
+          coordinate={{
+            latitude: val.coordinate.lat,
+            longitude: val.coordinate.lng
+          }}
+            onPress={() => {
+              setlocation({
+                
+              })
+            } }
+          onDragEnd={e => {
+              console.log('dragEnd', e.nativeEvent.coordinate);
+            }}
+          // draggable={true}
+          // tappable={true}
+          
+        >
+          <Image source={require("./asset/marker.png")}
+            style={{ width: 50, height: 50 }}
+            resizeMode='contain'
+            resizeMethod='resize'
+          />
+          <Callout tooltip>
+            <View>
+                <View style={styles.bubble}>
+                  <Text>{val.name} </Text>
+                  <Text>{ val.descritption}</Text>
+                </View>
+            </View>
+          </Callout>
+          </Marker>
+        ))}
+        
+
+      </MapView>
+      <View style={ styles.slideContainer}>
+        <Swiper showsPagination={false} style={styles.swiper}>
+          {markers.map((val, i) => (
+            <View key={i.toString()}>
+              <Text>{ val.name}</Text>
+              <Text>{ val.descritption}</Text>  
+              <TouchableOpacity style={{
+                height: 100, width: 100, backgroundColor: 'green', display: 'flex',justifyContent:'center',alignItems:'center'
+              }}
+                onPress={() => {
+                  setlocation({
+                    lng: val.coordinate.lng,
+                    lat:val.coordinate.lat
+                })
+              }}
+              >
+                <Text>HI</Text>
+              </TouchableOpacity>
+            </View>
+            ))}
+        </Swiper>   
+      
+      </View>
+         
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'flex-end',
     alignItems: 'center',
-    justifyContent: 'center',
+    
   },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+  bubble: {
+    flexDirection: 'row',
+    alignSelf: 'flex-start',
+    backgroundColor: '#fefefe',
+    borderRadius: 6,
+    borderColor: '#666',
+    borderWidth: 0.5,
+    padding: 15,
+    width: 150,
+  },
+  
+  swiper: {
+    paddingHorizontal: 20,
+    backgroundColor: 'white',
+    height:'100%'
+  },
+  slideContainer: {
+    
+    width: '100%',
+    height: 250,
+    padding:10
+  }
+
 });
---
+++ data/markers.js
@@ -0,0 +1,26 @@
+export const markers = [
+    {
+        coordinate: {
+            lat: 10.3016,
+            lng:123.8705
+        },
+        name: "Tisa Cebu City",
+        descritption:"Siomaian Sa Tisa"
+    },
+    {
+        coordinate: {
+            lat: 10.2873,
+            lng:123.8795
+        },
+        name: "Mambaling",
+        descritption:"Shopwise"
+    },
+    {
+        coordinate: {
+            lat: 10.2990,
+            lng:123.8813
+        },
+        name: "Labangon",
+        descritption:"Daghan Checkpoint"
+    }
+]
